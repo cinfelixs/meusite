2e30dece521d56fe0adca2078ee9efc70af62a9e
Carolina Felix <53052042+cinfelixs@users.noreply.github.com>
2019-11-26 21:25:34 -0300

    Update blockchain.js

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0x6E7C0d02485439033a8c739c1f92295158b4fCC3";
+var enderecoContrato = "0x3ED1b79d0a9B459274806458f17b41D872dF8ADD";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
@@ -38,6 +38,51 @@ function buscaStatusContrato() {
     contrato.statusPagamentoAluguel()
     .then( (resultado) => {
         campoStatus.innerHTML = resultado;
+    })
+    .catch( (err) => {
+        console.error(err);
+        campoStatus.innerHTML = err;
+    });
+}
+
+function encerrarContrato()
+{
+    var textoEncerrar = document.getElementById("encerrarContratoTx");
+
+    textoEncerrar.innerHTML="conectando para encerramento de contrato ...";
+    contrato.fimDoContrato()
+    .then( (transacao)=>
+    {
+            console.log("encerrarContrato - Transacao", transacao);
+            textoEncerrar.innerHTML="encerrando o contrato ...";
+            transacao.wait()
+            .then((resultado)=>
+            {
+                buscaFimContrato();
+                //textoEncerrar.innerHTML="contrato encerrado....";
+            })
+            .catch((err) =>
+            {
+                console.error("encerrarContrato - Aguardando tx ser minerada");
+                console.error(err);
+                textoEncerrar.innerHTML="erro ao se conectar minerar ....";
+            })
+     })
+     .catch((err)=>
+     {
+            console.error("encerrarContrato - Aguardando tx ser minerada");
+            console.error(err);
+            textoEncerrar.innerHTML="erro ao se conectar minerar ....";
+     })
+}
+
+function buscaFimContrato() {
+    var status;
+    var campoStatus = document.getElementById("encerrarContratoTx");     
+    contrato.contratoAtivo()
+    .then( (resultado) => 
+    {
+        campoStatus.innerHTML = "contrato encerrado";
     })
     .catch( (err) => {
         console.error(err);
